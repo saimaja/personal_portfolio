4640f1829683eeb65efb372c20e74832dafbef35
saimaja <saimaakhtar@gmail.com>
2020-08-22 19:00:24 -0400

    links working

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Projects from './Projects';
 import About from './About';
 import CV from './CV';
 import Contact from './Contact';
+import Research from './Research';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import { Container, Navbar, Nav } from 'react-bootstrap'
-import { Loader } from 'semantic-ui-react'
-import pic from './images/saimaprofile.png'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+// import { Grid, Image, Segment } from 'semantic-ui-react'
+// import pic from './images/saimaprofile.png'
 
 
 
@@ -16,24 +17,49 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar style={{paddingTop:'35px', paddingLeft: '80px', paddingRight: '80px', paddingBottom: '35px'}} collapseOnSelect expand="lg" >
-          <Navbar.Brand href="#home">Saima Akhtar</Navbar.Brand>
+        <Navbar style={{ paddingTop: '35px', paddingLeft: '50px', paddingRight: '50px', paddingBottom: '35px' }} collapseOnSelect expand="lg" >
+          <Navbar.Brand href="/">Saima Akhtar</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
             </Nav>
             <Nav>
-              <Nav.Link href="#features">About</Nav.Link>
-              <Nav.Link href="#pricing">CV</Nav.Link>
-              <Nav.Link href="#deets">Projects</Nav.Link>
-              <Nav.Link href="#memes">Contact</Nav.Link>
+              {/* <Nav.Link href="about">About</Nav.Link> */}
+              <Nav.Link href="cv">CV</Nav.Link>
+              <Nav.Link href="research">Research</Nav.Link>
+              <Nav.Link href="projects">Projects</Nav.Link>
+              <Nav.Link href="contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-       <div>
-        <img className='image-component' src={pic} alt={pic}/>
+
+        <Router>
+          <Switch>
+            <Route exact path='/' render={() =>
+              <About />} />
+
+            {/* <Route exact path='/about' render={() =>
+              <About />} /> */}
+
+            <Route exact path='/cv' render={() =>
+              <CV />} />
+
+            <Route exact path='/research' render={() =>
+              <Research />} />
+
+            <Route exact path='/projects' render={() =>
+              <Projects />} />
+
+            <Route exact path='/contact' render={() =>
+              <Contact />} />
+
+          </Switch>
+        </Router>
       </div>
-      </div>
+
+
+
+
 
     )
   }
--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import { Grid } from 'semantic-ui-react'
 
 export default class Projects extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <div> 
+        <Grid style={{paddingTop: '35px', paddingLeft: '50px', paddingRight: '50px'}} stackable columns={2}>
+          <Grid.Column>
+            {/* <Segment> */}
+              Description
+            {/* </Segment> */}
+          </Grid.Column>
+          <Grid.Column>
+            {/* <Segment> */}
+            I am a full stack developer with a passion for human-oriented design and research. As a trained architect and urban historian, the questions that drive me are at the intersection of technology and architecture -- whether through modelling buildings in 3D, designing web platforms for urban research, or thinking about the philosophical implications of using technology to augment the built environment. I bring strong skills in creative problem solving, project management, teamwork, and critical thinking to help institutions to build better, more socially-minded technology.
+            {/* </Segment> */}
+          </Grid.Column>
+        </Grid>
+        </div>
         )
     }
 }
