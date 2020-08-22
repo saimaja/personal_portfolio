205dec3df27a9c187a2ddfb447403642e5c9d0e1
saimaja <saimaakhtar@gmail.com>
2020-08-22 17:01:55 -0400

    laanding page

--- src/App.js
+++ src/App.js
@@ -6,32 +6,34 @@ import CV from './CV';
 import Contact from './Contact';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 import { Loader } from 'semantic-ui-react'
+import pic from './images/saimaprofile.png'
 
 
 
 export default class App extends Component {
   render() {
     return (
-
-      <Navbar collapseOnSelect expand="lg" >
-        <Navbar.Brand href="#home">Saima Akhtar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">About</Nav.Link>
-            <Nav.Link href="#pricing">CV</Nav.Link>
-
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">Projects</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Contact
-      </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div>
+        <Navbar style={{paddingTop:'35px', paddingLeft: '80px', paddingRight: '80px', paddingBottom: '35px'}} collapseOnSelect expand="lg" >
+          <Navbar.Brand href="#home">Saima Akhtar</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav>
+              <Nav.Link href="#features">About</Nav.Link>
+              <Nav.Link href="#pricing">CV</Nav.Link>
+              <Nav.Link href="#deets">Projects</Nav.Link>
+              <Nav.Link href="#memes">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+       <div>
+        <img className='image-component' src={pic} alt={pic}/>
+      </div>
+      </div>
 
     )
   }
