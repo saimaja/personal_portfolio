0d9ec1facbfb6aac484012241e5b46336df11a13
saimaja <saimaakhtar@gmail.com>
2020-08-22 14:44:20 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+// import logo from './logo.svg';
+import Projects from './Projects';
+import About from './About';
+import CV from './CV';
+import Contact from './Contact';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Loader } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  render() {
+    return (
+
+      <Navbar collapseOnSelect expand="lg" >
+        <Navbar.Brand href="#home">Saima Akhtar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">CV</Nav.Link>
+
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Projects</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Contact
+      </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+    )
+  }
 }
 
-export default App;
---
+++ src/CV.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CV extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
