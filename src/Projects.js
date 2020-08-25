55d05bb54061236ff3497069402b442935d47817
saimaja <saimaakhtar@gmail.com>
2020-08-25 19:53:41 -0400

    projects page

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Grid, Reveal, Image } from 'semantic-ui-react'
-import { View, Mask, MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import elicit from './images/elicit.png'
 import IFOTF from './images/IFOTF.png'
 import monuments from './images/monuments.png'
@@ -11,68 +11,77 @@ import './App.css'
 export default class Projects extends Component {
     render() {
         return (
-            <MDBContainer className="mt-5">
+            <MDBContainer className="mt-4">
             <MDBRow>
+
               <MDBCol md="4">
-                <MDBView hover>
+                <MDBView hover zoom>
                   <img
-                    src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                    src={multitudes}
                     className="img-fluid"
-                    alt=""
-                  />
-                  <MDBMask className="flex-center" overlay="red-strong">
-                    <p className="white-text">Strong overlay</p>
+                    alt="" />
+                  <MDBMask className="flex-center"
+                   overlay = 'black-strong' >
+                    <p className="white-text">Multitudes</p>
                   </MDBMask>
                 </MDBView>
               </MDBCol>
-              <MDBCol md="4">
-                <MDBView hover>
-                  <img
-                    src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                  <MDBMask className="flex-center" overlay="red-light">
-                    <p className="white-text">Light overlay</p>
-                  </MDBMask>
-                </MDBView>
-              </MDBCol>
-              <MDBCol md="4">
-                <MDBView hover>
-                  <img  src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
-                className="img-fluid"
-                alt=""
-              />
-              <MDBMask className="flex-center" overlay="red-slight">
-                <p className="white-text">Super light overlay</p>
-              </MDBMask>
-            </MDBView>
-          </MDBCol>
-        </MDBRow>
-        <MDBRow className="mt-4">
-          <MDBCol md="6">
+            
+          <MDBCol md="4">
             <MDBView hover zoom>
               <img
-                src="https://mdbootstrap.com/img/Others/documentation/img%20(131)-mini.jpg"
+                src={elicit}
                 className="img-fluid"
                 alt=""
               />
-              <MDBMask className="flex-center">
-                <p className="white-text">Zoom effect</p>
+              <MDBMask className="flex-center"
+              overlay = 'black-strong'>
+                <p className="white-text">E-licit Art</p>
               </MDBMask>
             </MDBView>
           </MDBCol>
-          <MDBCol md="6">
-            <h5 className="text-center">Shadow effect</h5>
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/4.jpg"
-              className="img-fluid rounded-circle hoverable"
-              alt=""
-            />
-           
-          </MDBCol>
-        </MDBRow>
-     
+
+          <MDBCol md="4">
+                <MDBView hover zoom>
+                  <img
+                    src={trivia}
+                    className="img-fluid"
+                    alt="" />
+                  <MDBMask className="flex-center"
+                   overlay = 'black-strong' >
+                    <p className="white-text">Trivia</p>
+                  </MDBMask>
+                </MDBView>
+              </MDBCol>
+              </MDBRow>
+
+              <MDBRow className="mt-5">
+              <MDBCol md="4">
+                <MDBView hover zoom>
+                  <img
+                    src={monuments}
+                    className="img-fluid"
+                    alt="" />
+                  <MDBMask className="flex-center" 
+                   overlay = 'black-strong'>
+                    <p className="white-text">Monuments</p>
+                  </MDBMask>
+                </MDBView>
+              </MDBCol>
+
+              <MDBCol md="4">
+                <MDBView hover zoom>
+                  <img
+                    src={IFOTF}
+                    className="img-fluid"
+                    alt="" />
+                  <MDBMask className="flex-center"
+                   overlay = 'black-strong' >
+                    <p className="white-text">In Front of the Factory</p>
+                  </MDBMask>
+                </MDBView>
+              </MDBCol>
+        </MDBRow> 
       </MDBContainer>
   
   
