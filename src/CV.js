01407ba411e0f764051cb4728d93b5c4bc3da939
saimaja <saimaakhtar@gmail.com>
2020-08-28 23:21:10 -0400

    cv

--- src/CV.js
+++ src/CV.js
@@ -13,8 +13,8 @@ export default class CV extends Component {
                         <span className='cv-header'>Postdoctoral Associate</span>
                         <br />
                             Yale University, Department of Computer Science <br />
-                        <span className='cv-subheader'> New Haven, CT | 2016-2019</span>
-                        <br />
+                        <span className='cv-subheader' style={{paddingBottom: '10px'}}> New Haven, CT | 2016-2019</span>
+                      
                         <span>Managed cultural heritage preservation projects between the fields of computer science and architecture. Oversaw a team of
                             computer science students and researchers to develop <i>Anqa</i>, an open access cultural heritage platform that documented six sites in Damascus, Syria. 
                             Spearheaded workshops, research, design, and wireframing, with an emphasis on empathy and storytelling from the ground up. To learn more, click
@@ -24,8 +24,8 @@ export default class CV extends Component {
                         <span className='cv-header'>Curator</span>
                         <br />
                             Richard F. Brush Gallery, St. Lawrence University <br />
-                        <span className='cv-subheader'> Canton, NY | Fall 2017</span>
-                        <br />
+                        <span className='cv-subheader' style={{paddingBottom: '10px'}}> Canton, NY | Fall 2017</span>
+                       
                         <span>Co-curated <a href='https://www.stlawu.edu/content/multitudes-art-exhibition-age-muslimban' rel="noopener noreferrer" target='_blank'>Multitudes: An Art Exhibit in the Age of the #muslimban</a>,
                             an exhibition aimed at transcending narrow representations of Muslims in popular culture.
                             The selected work reflects eight artists' cutting-edge and subversive commentaries
@@ -37,8 +37,8 @@ export default class CV extends Component {
                         <br />
                             Gobee Group 
                         <br />
-                        <span className='cv-subheader'> Oakland, CA | 2014</span>
-                        <br />
+                        <span className='cv-subheader' style={{paddingBottom: '10px'}}> Oakland, CA | 2014</span>
+                      
                         <span> Conducted research based on design thinking for <a href='https://www.cell-ed.com/' rel="noopener noreferrer" target='_blank'>Cell-ED</a>, an English-as-a-Second-Language (ESL) educational 
                             startup for adults. Wrote briefs and <a href='https://gobeefellows.wordpress.com/2014/08/04/an-education/' rel="noopener noreferrer" target='_blank'>literacy analyses </a> 
                             that focused on improving access to English learning for adults in upstate New York, and assisted in improving pedagogical design.</span>
@@ -53,14 +53,14 @@ export default class CV extends Component {
                         <span className='cv-header'>University of California - Berkeley</span>
                         <br />
                             Doctor of Philosophy (Ph.D.), Department of Architecture
-                        <span className='cv-subheader'> Designated Emphasis: Global Metropolitan Studies<br />
-                             Dissertation: <i>Corporate Empire: Fordism and the Making of Immigrant Detroit</i></span>
+                        <span className='cv-subheader' style={{lineHeight: '1.5'}}> Designated Emphasis: Global Metropolitan Studies<br />
+                        Dissertation: <i>Corporate Empire: Fordism and the Making of Immigrant Detroit</i></span>
                         <br />
 
                         <span className='cv-header'>Massachusetts Institute of Technology</span>
                         <br />
                             Master of Science (M.Sc.), Architecture
-                        <span className='cv-subheader'>
+                        <span className='cv-subheader' style={{lineHeight: '1.5'}}>
                             Thesis: <i>Shangri La: Architecture as Collection</i></span>
                         <br />
 
@@ -83,20 +83,15 @@ export default class CV extends Component {
                     </Grid.Column>
 
                     <Grid.Column style={{ width: '30%' }} className='cv-caption-text'>
-                        FELLOWSHIPS + AWARDS
+                        FELLOWSHIPS + GRANTS
                     </Grid.Column>
                     <Grid.Column style={{ width: '60%', textAlign: 'justify' }} className='text'>
-                        <span className='cv-header'>Public Voices Fellowship</span>
-                        <br />
-                        <span className='cv-subheader'> Yale University | New Haven, CT | 2018-2019</span>
-                        <br />
-                        <span className='cv-header'>Irmgard Coninx Fellowship</span>
-                        <br />
-                        <span className='cv-subheader'>ICI Berlin/Forum Transregionale Studien | Berlin, Germany | 2015-2016</span>
-                        <br />
-                        <span className='cv-header'>AIA Research Fellowship</span>
-                        <br />
-                        <span className='cv-subheader'> American Institute of Architects (Academy of Neuroscience for Architecture) | Washington, DC | 2004-2005</span>
+                        <span className='text'>Public Voices Fellowship </span>
+                        <span className='cv-subheader' style={{paddingBottom: '10px'}}>Yale University | New Haven, CT | 2018-2019</span>
+                        <span className='text'>Irmgard Coninx Fellowship </span>
+                        <span className='cv-subheader' style={{paddingBottom: '10px'}}>ICI Berlin/Forum Transregionale Studien | Berlin, Germany | 2015-2016</span>
+                        <span className='text'>AIA Research Fellowship</span>
+                        <span className='cv-subheader' style={{paddingBottom: '10px'}}> American Institute of Architects (Academy of Neuroscience for Architecture) | Washington, DC | 2004-2005</span>
                     </Grid.Column>
 
                     <Grid.Column style={{ width: '30%' }} className='cv-caption-text'>
